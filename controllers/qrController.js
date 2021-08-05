e943cf241daafc73240a5402e70e4467ba47dc68
weiliangchin3 <weiliangchin3@gmail.com>
2021-08-05 14:31:03 +0800

    Update qrController.js

--- controllers/qrController.js
+++ controllers/qrController.js
@@ -1,7 +1,7 @@
 const firebase = require('../db');
 const Qr = require('../models/qr');
 const firestore = firebase.firestore();
-const voucherController = require('./voucherController');
+
 
 const createqrcode = async (req, res, next) => {
     try {
@@ -11,8 +11,33 @@ const createqrcode = async (req, res, next) => {
         const data = req.body;
         const updatedVoucher = await firestore.collection('users').doc(userId).collection('merchants').doc(merchId).collection('vouchers').doc(vouchId);
         await updatedVoucher.update(data);
-        const createQR = await firestore.collection('users').doc(userId).collection('merchants').doc(merchId).collection('vouchers').doc(vouchId).collection('qr').doc().set(data);
-        await createQR.update(data);
+        const datas = await firestore.collection('users').doc(userId).collection('merchants').doc(merchId).collection('vouchers').doc(vouchId);
+        await uallVouchersSnapshot.update(datas).get();
+        allVouchersSnapshot.forEach(vouch => {
+            const voucher = new Voucher(
+                merchId,
+                vouch.id,
+                vouch.data().value,
+                vouch.data().qty,
+                vouch.data().price
+            );
+            voucherArr.push(voucher);
+        })
+        const createQR = await firestore.collection('users').doc(userId).collection('merchants').doc(merchId).collection('vouchers').doc(vouchId).collection('qr').doc().set(voucherarr);
+        await createQR.update(voucherarr);
+        res.send("QR code created successfully updated");
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const updateqrcode = async (req, res, next) => {
+    try {
+        const qrId = req.params.id; //Merch ID
+        const userId = req.params.userId;
+        const data = req.body;
+        const updatedQR = await firestore.collection('users').doc(userId).collection('merchants').doc(merchId).collection('vouchers').doc(vouchId).collection('qr').doc(qrId);
+        await updatedQR.update(data);
         res.send("QR code created successfully updated");
     } catch (error) {
         res.status(400).send(error.message);
@@ -23,6 +48,6 @@ const createqrcode = async (req, res, next) => {
 
 
 module.exports = {
-    createqrcode
+    createqrcode, updateqrcode
 }
 
