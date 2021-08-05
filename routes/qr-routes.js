01549b8c611aa537689697a7367a2ebb96e276db
weiliangchin3 <weiliangchin3@gmail.com>
2021-08-05 14:30:20 +0800

    Update qr-routes.js

--- routes/qr-routes.js
+++ routes/qr-routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createqrcode } = require('../controllers/qrController');
+const { createqrcode, updateqrcode } = require('../controllers/qrController');
 
 const router = express.Router();
 
 router.put('/qr/:userId/:id/:vouchId', createqrcode);
+router.put('/qr/:userId/:qrId', updateqrcode);
+
 
 module.exports = {
     routes: router
